e6ec1f176e9a74a9975f30f6a740a472bdd64e1b
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-04-19 16:09:13 +0100

    Made some adjustments to the login page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+    res.render('template');
+});
+
+router.post('/template', function (req, res, next) {
+    // If passward is equal to"password"
+    if (req.body.Password == "password") {
+        res.render('template', { Name: req.body.Name, Password: req.body.Password });
+        //getText();
+    }
+    else {
+        res.redirect('error');
+    }
 });
 
 module.exports = router;
