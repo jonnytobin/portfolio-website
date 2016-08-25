52ee213f5fa58bcdf6622c25b9e69ce8bdc36949
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-08-25 23:39:21 +0100

    Setting up mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoClient = require('mongodb').MongoClient;
+var url = process.env.CUSTOMCONNSTR_MongoDB || 'mongodb://localhost:27017/test';
+
 /* GET Login page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
@@ -22,9 +25,8 @@ router.post('/template', function (req, res, next) {
     }
 });
 
+// When a file is uploaded open the file uploaded page to confirm to the user
 router.post('/upload', function(req, res, next) {
-  // req.files contains all the information about the files that
-  // have been uploaded, lets print it out and see what is in it
   console.log(req.files);
   res.render('/files/file_uploaded', {uploadedFile: req.files[0].originalname});
 });
